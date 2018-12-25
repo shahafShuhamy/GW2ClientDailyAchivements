e030638bb54a5c0b930aaaf9cacc3e59e3e46005
ShahafShuhamy <Shahaf761@gmail.com>
2018-12-25 16:27:04 +0200

    changed slide animation animate timing

--- src/animations/slide-in.animation.ts
+++ src/animations/slide-in.animation.ts
@@ -4,9 +4,9 @@ export const slideInOut =
 trigger('slideInOut', [
   transition(':enter', [
     style({transform: 'translateX(-100%)'}),
-    animate('0.5s 300ms ease-in', style({transform: 'translateX(0%)'}))
+    animate('0.75s 300ms ease-in', style({transform: 'translateX(0%)'}))
   ]),
   transition(':leave', [
-    animate('0.5s 300ms ease-in', style({transform: 'translateX(-100%)'}))
+    animate('0.75s 300ms ease-in', style({transform: 'translateX(-100%)'}))
   ])
 ]);
